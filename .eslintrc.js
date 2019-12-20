7ca753c1430c9049bb89002cd4d503d40f32dd9a
Kito Clark <kclark@bridj.com>
2019-12-20 20:29:16 +1000

    Fixed  eslint, tslint and prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,23 @@
 module.exports = {
-    env: {
-        es6: true,
-        node: true,
-    },
-    extends: [
-        'airbnb-base',
-        'plugin:@typescript-eslint/recommended',
-        'prettier/@typescript-eslint',
-        'plugin:prettier/recommended',
-    ],
-    globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly',
-    },
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: 'module',
-    },
-    plugins: ['@typescript-eslint'],
-    rules: {},
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+    "airbnb"
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["@typescript-eslint"],
+  rules: { "arrow-parens": "ignore" }
 };
