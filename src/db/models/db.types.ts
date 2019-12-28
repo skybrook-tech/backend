c98a89905256c237c783b585b28d5b9c339d88d4
Kito Clark <kclark@bridj.com>
2019-12-28 20:02:43 +1000

    added Users model type to Db

--- src/db/models/db.types.ts
+++ src/db/models/db.types.ts
@@ -1,6 +1,8 @@
 import { Sequelize } from "sequelize";
+import { UsersModelStatic } from "./users/types";
 
 export interface Db {
   sequelize: Sequelize;
   Sequelize: any;
+  Users: UsersModelStatic;
 }
