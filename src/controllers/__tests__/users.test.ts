c3f726fb75ee896e4d224dfd62c582af0aa24913
Kito Clark <kclark@bridj.com>
2019-12-29 08:55:57 +1000

    refactored user controller tests and added test for successful registatrion

--- src/controllers/__tests__/users.test.ts
+++ src/controllers/__tests__/users.test.ts
@@ -9,7 +9,12 @@ import dbUtils from "../../utils/test-utils/db";
 const router = express.Router();
 const app = setupServerDefaults({ routes: router.use("/users", controllers.users) });
 
-const existingUser = { email: "existing@email.com" as string, password: "password" as null };
+interface UserType {
+  email: string;
+  password: string;
+}
+
+const existingUser = { email: "existing@email.com", password: "password" } as UserType;
 
 beforeAll(async () => {
   await dbUtils.migrate();
@@ -24,42 +29,72 @@ afterAll(async () => {
   await db.Users.destroy({ where: {}, truncate: true });
 });
 
-describe("users controller", () => {
-  describe("POST /users/register", () => {
-    describe("validations", () => {
-      [
-        {
-          description: "sends error when no email provided",
-          expected: errors.authentication.AUTH_NO_P_OR_U,
-          requestBody: { email: null as null, password: "password" as string }
-        },
-        {
-          description: "sends error when no password provided",
-          expected: errors.authentication.AUTH_NO_P_OR_U,
-          requestBody: { email: "foo" as string, password: null as null }
-        },
-        {
-          description: "sends error when user already exists",
-          expected: errors.authentication.AUTH_USER_EXISTS,
-          requestBody: existingUser,
-          beforeFunction: (app: any) => {
-            return;
-          }
-        }
-      ].forEach(({ description, expected, requestBody, beforeFunction }) => {
-        it(description, async done => {
-          if (beforeFunction) await beforeFunction;
+const testCases = {
+  validations: [
+    {
+      description: "sends error when no email provided",
+      expected: errors.authentication.AUTH_NO_P_OR_U,
+      requestBody: { email: null, password: "password" } as UserType
+    },
+    {
+      description: "sends error when no password provided",
+      expected: errors.authentication.AUTH_NO_P_OR_U,
+      requestBody: { email: "foo", password: null } as UserType
+    },
+    {
+      description: "sends error when user already exists",
+      expected: errors.authentication.AUTH_USER_EXISTS,
+      requestBody: existingUser,
+      beforeFunction: (app: any) => {
+        return;
+      }
+    }
+  ]
+};
 
-          request(app)
-            .post("/users/register")
-            .set("Accept", "application/json")
-            .send(requestBody)
-            .expect("Content-Type", /json/)
-            .expect({ error: expected })
-            .expect(expected.status)
-            .end(done);
-        });
-      });
+describe("users controller --- POST '/users/register' --- validations", () => {
+  const testFunc = ({
+    description,
+    expected,
+    requestBody,
+    beforeFunction
+  }: {
+    description: string;
+    expected: any;
+    requestBody: any;
+    beforeFunction: () => void;
+  }) => {
+    it(description, async done => {
+      if (beforeFunction) await beforeFunction;
+
+      request(app)
+        .post("/users/register")
+        .set("Accept", "application/json")
+        .send(requestBody)
+        .expect("Content-Type", /json/)
+        .expect({ error: expected })
+        .expect(expected.status)
+        .end(done);
     });
+  };
+
+  testCases.validations.forEach(testFunc);
+});
+
+describe("users controller --- POST '/users/register' --- success", () => {
+  const newUserAccount = { email: "new_user@email.com", password: "password" } as UserType;
+  const expected = { status: 200 };
+
+  it("creates a new user account and response with jwt", async done => {
+    request(app)
+      .post("/users/register")
+      .set("Accept", "application/json")
+      .send(newUserAccount)
+      .expect(res => {
+        expect(res.body.token).toBeTruthy();
+      })
+      .expect("Content-Type", /json/)
+      .expect(expected.status)
+      .end(done);
   });
 });
