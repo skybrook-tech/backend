e44338a3ddd0dc28d55d143a28e677f70d57d707
Kito Clark <kclark@bridj.com>
2019-12-29 09:49:09 +1000

    added auth login errors and types

--- src/errors/authentication.ts
+++ src/errors/authentication.ts
@@ -1,3 +1,12 @@
+import { ErrorResponse } from "./types";
+
+interface AuthErrorTypes {
+  AUTH_NO_P_OR_U: ErrorResponse;
+  AUTH_USER_EXISTS: ErrorResponse;
+  AUTH_USER_NOT_FOUND: ErrorResponse;
+  AUTH_USER_WRONG_PW: ErrorResponse;
+}
+
 const authErrors = {
   AUTH_NO_P_OR_U: {
     message: "No username or password provided.",
@@ -8,7 +17,17 @@ const authErrors = {
     message: "An account with that email already exists.",
     status: 401,
     code: "AUTH_USER_EXISTS"
+  },
+  AUTH_USER_NOT_FOUND: {
+    message: "That account does not exist.",
+    status: 401,
+    code: "AUTH_USER_NOT_FOUND"
+  },
+  AUTH_USER_WRONG_PW: {
+    message: "The password you have provided is incorrect.",
+    status: 401,
+    code: "AUTH_USER_WRONG_PW"
   }
-};
+} as AuthErrorTypes;
 
 export default authErrors;
