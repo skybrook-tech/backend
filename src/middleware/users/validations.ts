ca2bd5d918ebbd33d9bee63d43684fa60526b3cd
Kito Clark <kclark@bridj.com>
2019-12-29 09:49:28 +1000

    added login validations

--- src/middleware/users/validations.ts
+++ src/middleware/users/validations.ts
@@ -21,6 +21,25 @@ const registration = async (req: Request, res: Response, next: NextFunction) => 
   }
 };
 
-const validations = { registration };
+const login = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return next(errors.authentication.AUTH_NO_P_OR_U);
+    }
+
+    const user = await db.Users.findOne({ where: { email } });
+
+    if (!user) {
+      return next(errors.authentication.AUTH_USER_NOT_FOUND);
+    }
+
+    next();
+  } catch (error) {
+    return next(error);
+  }
+};
+
+const validations = { registration, login };
 
 export default validations;
