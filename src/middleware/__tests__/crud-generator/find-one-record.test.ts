9d731201d3f0c3f7dabb87c64791d48c1f6e1639
Kito Clark <kclark@bridj.com>
2019-12-31 11:14:39 +1000

    created testModel factory and added to find-one-record test

--- src/middleware/__tests__/crud-generator/find-one-record.test.ts
+++ src/middleware/__tests__/crud-generator/find-one-record.test.ts
@@ -1,23 +1,6 @@
-import createRecord from "../../crud-generator/create-record";
 import findOneRecord from "../../crud-generator/find-one-record";
 import db from "../../../db/models";
-
-const createRecordForTest = () => {
-  const include = [{ model: db.__testModelRelated__ }];
-
-  const locals = { sequelizeParams: { include } };
-  const expectedRelatedValue = "related Value one";
-
-  const body = {
-    attr1: "valueOne",
-    attr2: 1,
-    attr3: true,
-    __testModelRelated__s: [{ attr1: expectedRelatedValue }],
-    foo: "bar"
-  };
-
-  return createRecord({ Model: db.__testModel__, locals, body });
-};
+import createTestModel from "../../../factories/__testModel__";
 
 afterAll(async () => {
   await db.__testModel__.destroy({ where: {} });
@@ -25,7 +8,7 @@ afterAll(async () => {
 
 describe("crud-generator --- find-one-record -- basic", () => {
   it("returns record with provided id", async () => {
-    const expected = await createRecordForTest();
+    const expected = await createTestModel();
 
     const params = { id: expected.id };
     const record = await findOneRecord({ Model: db.__testModel__, params });
@@ -40,7 +23,9 @@ describe("crud-generator --- create-record -- with related and sequelizeParams",
 
     const locals = { sequelizeParams: { include } };
 
-    const expected = await createRecordForTest();
+    const expected = await createTestModel({
+      props: { __testModelRelated__s: [{ attr1: "random" }] }
+    });
 
     const params = { id: expected.id };
     const record = await findOneRecord({ Model: db.__testModel__, params, locals });
