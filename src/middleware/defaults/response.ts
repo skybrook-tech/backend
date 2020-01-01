be8dd362163ff0ccb6efd173aff74690836c2ebb
Kito Clark <kclark@bridj.com>
2020-01-02 09:59:39 +1000

    added contebnt-type header settings to defaultResponse

--- src/middleware/defaults/response.ts
+++ src/middleware/defaults/response.ts
@@ -1,6 +1,7 @@
 import * as express from "express";
 
 const defaultResponse = (req: express.Request, res: express.Response): void => {
+  res.set("Content-Type", "application/json");
   res.status(200).json(res.locals.response);
 };
 
