bd57aec64190f45be6268c68c2cad2bfc5c0049d
Kito Clark <kclark@bridj.com>
2020-01-02 10:11:03 +1000

    added CRUD middleware to Projects

--- src/controllers/projects.ts
+++ src/controllers/projects.ts
@@ -1,14 +1,18 @@
 "use strict";
 import createController from "../utils/create-controller";
+import middleware from "../middleware";
+import db from "../db/models";
+
+const ProjectsCrud = middleware.createCrudMiddleware(db.Projects);
 
 export default createController({
-  model: "projects",
+  model: "Projects",
   middleware: {
-    getOne: [],
-    getAll: [],
-    create: [],
-    destroy: [],
-    update: []
+    create: [ProjectsCrud.create],
+    getOne: [ProjectsCrud.findOne],
+    getAll: [ProjectsCrud.findAll],
+    update: [ProjectsCrud.update],
+    destroy: [ProjectsCrud.destroy]
   },
   nestedControllers: []
 });
--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,15 +1,23 @@
 import authentication from "./authentication";
+import createCrudMiddleware from "./crud-generator";
 import defaults from "./defaults/response";
 import errorHandler from "./error-handler";
 import users from "./users";
 
 interface MiddlewareObject {
   authentication: any;
+  createCrudMiddleware: any;
   defaults: any;
   errorHandler: any;
   users: any;
 }
 
-const middleware = { authentication, defaults, errorHandler, users } as MiddlewareObject;
+const middleware = {
+  authentication,
+  createCrudMiddleware,
+  defaults,
+  errorHandler,
+  users
+} as MiddlewareObject;
 
 export default middleware;
