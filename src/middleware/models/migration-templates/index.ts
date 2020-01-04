c2ee25bfcb6a4b75a8294bc79091e4a9f5d5c32e
Kito Clark <kclark@bridj.com>
2020-01-04 15:40:01 +1000

    added other migrations to import

--- src/middleware/models/migration-templates/index.ts
+++ src/middleware/models/migration-templates/index.ts
@@ -1,9 +1,17 @@
+import addColumn from "./add-column";
+import changeColumn from "./change-column";
 import createTable from "./create-table";
 import dropTable from "./drop-table";
+import removeColumn from "./remove-column";
+import renameColumn from "./rename-column";
 
 const migrationTemplates = {
+  addColumn,
+  changeColumn,
   createTable,
-  dropTable
+  dropTable,
+  removeColumn,
+  renameColumn
 };
 
 export default migrationTemplates;
