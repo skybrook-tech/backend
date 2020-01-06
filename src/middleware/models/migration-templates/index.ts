4b1b3a9b18dad7bf439e70a83cd2a3c93d8c7593
Kito Clark <kclark@bridj.com>
2020-01-06 13:01:49 +1000

    added types to migration templates

--- src/middleware/models/migration-templates/index.ts
+++ src/middleware/models/migration-templates/index.ts
@@ -4,6 +4,28 @@ import createTable from "./create-table";
 import dropTable from "./drop-table";
 import removeColumn from "./remove-column";
 import renameColumn from "./rename-column";
+import { MigrationArgs } from "../types";
+
+export interface MigrationTemplate {
+  projectId: number;
+  type: string;
+  name: string;
+  timeStamp: number;
+  isMigrated: boolean;
+  up: any;
+  down: any;
+}
+
+type MigrationTemplateFunction = (args: MigrationArgs) => MigrationTemplate;
+
+interface MigrationTemplates {
+  addColumn: MigrationTemplateFunction;
+  changeColumn: MigrationTemplateFunction;
+  createTable: MigrationTemplateFunction;
+  dropTable: MigrationTemplateFunction;
+  removeColumn: MigrationTemplateFunction;
+  renameColumn: MigrationTemplateFunction;
+}
 
 const migrationTemplates = {
   addColumn,
@@ -12,6 +34,6 @@ const migrationTemplates = {
   dropTable,
   removeColumn,
   renameColumn
-};
+} as MigrationTemplates;
 
 export default migrationTemplates;
