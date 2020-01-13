80c928bd3e230bee87815192d33d483f6d4e4176
Kito Clark <kclark@bridj.com>
2020-01-14 08:36:41 +1000

    updated submodule and added db to context middleware

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import _config from "../_config";
 import routes from "./routes";
 import setupServerDefaults from "./core/utils/setup-server-defaults";
+import middleware from "./middleware";
 
-const app = setupServerDefaults({ routes });
+const app = setupServerDefaults({ routes, globalMiddleware: [middleware.addDbToContext] });
 
 const PORT = process.env.NODE_ENV === "test" ? 3071 : _config.PORT;
 
